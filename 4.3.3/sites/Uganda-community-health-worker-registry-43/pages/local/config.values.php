6def3f788066f1cd1ebf2a24a4bfe1cca4400e75
Agaba Andrew <agabaandre@gmail.com>
2022-05-30 17:06:44 +0300

    version

--- 4.3.3/sites/Uganda-community-health-worker-registry-43/pages/local/config.values.php
+++ 4.3.3/sites/Uganda-community-health-worker-registry-43/pages/local/config.values.php
@@ -68,7 +68,7 @@ $i2ce_site_user_access_init = null;
 /**
  * the configuration xml file for the site module.  You need to set this.
  */
-$i2ce_site_module_config = "/var/lib/iHRIS/iHRIS_train_qualify/4.3.3/sites/Uganda-community-health-worker-registry.xml";
+$i2ce_site_module_config = "/var/lib/iHRIS/iHRIS_train_qualify/4.3.3/sites/Uganda-community-health-worker-registry-43/Uganda-community-health-worker-registry.xml";
 
 
 /*****************************************************************
