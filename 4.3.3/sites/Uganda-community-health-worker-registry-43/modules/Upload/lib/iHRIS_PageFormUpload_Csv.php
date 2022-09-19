ca9beeb46cace5d40bf6e4a84d5af69eb73168d2
AGABA ANDREW <agabaandre@gmail.com>
2022-09-20 00:25:34 +0300

    added upload template

--- 4.3.3/sites/Uganda-community-health-worker-registry-43/modules/Upload/lib/iHRIS_PageFormUpload_Csv.php
+++ 4.3.3/sites/Uganda-community-health-worker-registry-43/modules/Upload/lib/iHRIS_PageFormUpload_Csv.php
@@ -460,8 +460,8 @@ class iHRIS_PageFormUpload_Csv extends I2CE_PageFormCSV
 
         // Now we create the position.
         $position = $this->factory->createContainer("position");
-
-        //$position->getField('job')->setFromDB($job);
+        $person_position->setParent($person_id);
+        $position->getField('job')->setFromDB($job);
         $position->title = $this->current[$key]['row']['Position'];
         $position->getField('facility')->setFromDB($facility);
         // $position->getField('facility_office')->setFromDB($facility_office);
