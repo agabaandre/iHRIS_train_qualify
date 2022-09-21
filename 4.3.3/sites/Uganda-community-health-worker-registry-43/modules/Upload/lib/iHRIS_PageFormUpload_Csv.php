7310341999b865615d9207b407983efdbabfb32c
AGABA ANDREW <agabaandre@gmail.com>
2022-09-21 11:26:55 +0300

    upload new vhts

--- 4.3.3/sites/Uganda-community-health-worker-registry-43/modules/Upload/lib/iHRIS_PageFormUpload_Csv.php
+++ 4.3.3/sites/Uganda-community-health-worker-registry-43/modules/Upload/lib/iHRIS_PageFormUpload_Csv.php
@@ -33,7 +33,7 @@ class iHRIS_PageFormUpload_Csv extends I2CE_PageFormCSV
             }
             //$required_headers = array( 'ID Number', 'Status', 'Date/Time', 'VerifyCode', 'CardNo' );
             //$required_headers = array( 'Surname', 'Firstname','Othername','Gender','Birth Date','Marital Status','Position','Facility','Residence District','Home District','File Number','Employee/IPPS Number','Date of Current Appointment','Date of First Appointment','DSC Minute','Current Salary','Facility Office','Terms of Employment','Telephone Number', 'Email Address' );
-            $required_headers = array('National ID', 'Religion', 'Surname', 'Firstname', 'Othername', 'Gender', 'Birth Date', 'Marital Status', 'Position', 'Facility', 'Village Served', 'Residence District', 'Home District', 'Date of Current Appointment', 'Date of First Appointment',  'Current Salary',  'Terms of Employment', 'Mobile Number', 'Telephone Number');
+            $required_headers = array('National ID', 'Religion', 'Surname', 'Firstname', 'Othername', 'Gender', 'Birth Date', 'Marital Status', 'Position', 'Facility', 'Village Served', 'Recruitment Mechanism','Residence District', 'Home District', 'Date of Current Appointment', 'Date of First Appointment',  'Current Salary',  'Terms of Employment', 'Mobile Number', 'Telephone Number');
             $invalid_headers = array();
             foreach ($required_headers as $header) {
                 if (!in_array($header, $this->current['upload_csv']['header'])) {
@@ -154,7 +154,7 @@ class iHRIS_PageFormUpload_Csv extends I2CE_PageFormCSV
         $job = $this->lookupList("job", $this->current[$key]['row']['Position'], 'title');
         $facility = $this->lookupList("facility", $this->current[$key]['row']['Facility'], 'name');
         $village = $this->lookupList("village", $this->current[$key]['row']['Village Served'], 'name');
-        $recruit_mech = $this->lookupList("recruit_mech", $this->current[$key]['row']['Recruitment Mechanism'], 'name');
+        $recruit_mech = $this->lookupList("recruit_mech", $this->current[$key]['row']['Recruitment Mechanism']);
         $employment_terms = $this->lookupList("employment_terms", $this->current[$key]['row']['Terms of Employment']);
         //$facility_office = $this->lookupList("facility_office", $this->current[$key]['row']['Facility Office']);
         $marital_status = $this->lookupList("marital_status", $this->current[$key]['row']['Marital Status']);
